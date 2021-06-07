b692d948cecd0fd3e817371af62a79a285adf12d
cubOlson <cub.olson@gmail.com>
2021-06-07 19:00:50 -0400

    modal actually done but needs styling

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -1,16 +1,25 @@
 import ReactDOM from 'react-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import './modal.css'
 
+import { hideModal } from '../../store/modal'
+
 export default function Modal () {
+
+    const dispatch = useDispatch()
+
     const mount = useSelector(state => state.modal.mount);
     const display = useSelector(state => state.modal.display);
     const Current = useSelector(state => state.modal.current);
 
+    const onClose = () => {
+        dispatch(hideModal())
+    }
+
     return mount && display && ReactDOM.createPortal(
-        <div className='modal-background'>
-            <div className='modal-content'>
+        <div onClick={onClose} className='modal-background'>
+            <div onClick={ e => e.stopPropogation()} className='modal-content'>
                 <Current />
             </div>
         </div>
